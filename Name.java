1f483fb28648004cf3137e95cc7c14d8722ec6a0
Farrelias04 <wmredzafarrel@gmail.com>
2024-10-21 15:47:14 +0100

    Added java source code

---
+++ Name.java
@@ -0,0 +1,11 @@
+public class Name
+{
+	public static void main(String[] args)
+	{
+		String first = "Jon";
+        String middle = "Edward";
+		String last = "Rowe";
+		System.out.println(first + " " + middle + " " + last);
+	}
+	
+}
